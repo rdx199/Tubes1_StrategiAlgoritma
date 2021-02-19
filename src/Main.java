0dd8fe3e3f0b0843762d5b7df23a958961edc39f
Dheatly23 <71598333+Dheatly23@users.noreply.github.com>
2021-02-19 11:15:51 +0700

    Mengganti mainlooop

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,61 @@
-package za.co.entelect.challenge;
-
-import com.google.gson.Gson;
-import za.co.entelect.challenge.command.Command;
-import za.co.entelect.challenge.entities.GameState;
-
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Random;
 import java.util.Scanner;
 
+import org.json.JSONObject;
+
 public class Main {
+    private static class DummySelector extends MoveSelector {
+
+        public DummySelector() {
+        }
+
+        @Override
+        public boolean isStateBetter(State state, State other) {
+            return true;
+        }
+
+    }
 
     private static final String ROUNDS_DIRECTORY = "rounds";
     private static final String STATE_FILE_NAME = "state.json";
 
     /**
-     * Read the current state, feed it to the bot, get the output and print it to stdout
+     * Read the current state, feed it to the bot, get the output and print it
+     * to stdout
      *
      * @param args the args
      **/
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         Scanner sc = new Scanner(System.in);
-        Gson gson = new Gson();
-        Random random = new Random(System.nanoTime());
 
-        while (true) {
-            try {
+        try {
+            while (true) {
                 int roundNumber = sc.nextInt();
 
-                String statePath = String.format("./%s/%d/%s", ROUNDS_DIRECTORY, roundNumber, STATE_FILE_NAME);
-                String state = new String(Files.readAllBytes(Paths.get(statePath)));
+                try {
+                    String statePath = String.format("./%s/%d/%s",
+                            ROUNDS_DIRECTORY, roundNumber, STATE_FILE_NAME);
+                    String str = new String(
+                            Files.readAllBytes(Paths.get(statePath)));
+                    JSONObject json = new JSONObject(str);
+                    State state = new State(json);
 
-                GameState gameState = gson.fromJson(state, GameState.class);
-                Command command = new Bot(random, gameState).run();
+                    MoveIterator it = new SingleThreadedIterator();
+                    Command command = it.iterateMove(state,
+                            new DummySelector()); // TODO: Selector?
 
-                System.out.println(String.format("C;%d;%s", roundNumber, command.render()));
-            } catch (Exception e) {
-                e.printStackTrace();
+                    System.out.format("C;%d;%s\n", roundNumber,
+                            command.toString());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    System.out.format("C;%d;nothing\n", roundNumber);
+                }
             }
+        } finally {
+            sc.close();
         }
     }
 }
